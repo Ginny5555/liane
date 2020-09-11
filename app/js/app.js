c4d538d563e52380d4487114cef3c6dd8914d171
Yevheniia Spaska <yevheniiaspaska@MacBook-Pro-Yevheniia.local>
2020-09-11 19:26:16 +0300

    add  files

---
+++ app/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function($) {
+  $(".slider").slick({
+    centerMode: false,
+    draggable: false,
+    arrows: true,
+    autoplay: false,
+    autoplaySpeed: 200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true
+  });
+});
